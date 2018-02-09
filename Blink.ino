f570df7eeefa077c36ddd5d3a4c7c288ab2d5f51
Tanvir Fahim <tanvircsedu15@gmail.com>
2018-02-09 17:44:18 +0600

    Add files via upload

---
+++ Blink.ino
@@ -0,0 +1,44 @@
+int arr[] = {6, 7, 8, 9, 10, 11, 12, 13};
+void setup() {
+  for (int i = 0; i < 8; i++)
+  {
+    pinMode(arr[i], OUTPUT);
+  }
+}
+void loop() {
+  for (int i = 0; i < 8; i++)
+  {
+    digitalWrite(arr[i], HIGH);
+    delay(200);
+  }
+  delay(500);
+  for (int i = 0; i < 8; i++)
+  {
+    digitalWrite(arr[i], LOW);
+    delay(200);
+  }
+  delay(500);
+  for (int i = 0; i < 8; i++)
+  {
+    digitalWrite(arr[i], HIGH);
+    delay(500);
+    digitalWrite(arr[i], LOW);
+  }
+  delay(500);
+  for (int i = 0; i < 8; i++)
+  {
+    digitalWrite(arr[7 - i], HIGH);
+    delay(500);
+    digitalWrite(arr[7 - i], LOW);
+  }
+  delay(500);
+  for (int i = 0; i < 4; i++)
+  {
+    digitalWrite(arr[i], HIGH);
+    digitalWrite(arr[i+4], HIGH);
+    delay(500);
+    digitalWrite(arr[i], LOW);
+    digitalWrite(arr[i+4], LOW);
+  }
+  delay(500);
+}

3b1b3eb049c06548debb2774c18b1dc9d8b561a5
Tanvir Fahim <tanvircsedu15@gmail.com>
2018-02-22 22:19:51 +0600

    Add files via upload

---
+++ toneKeyboard/toneKeyboard.ino
@@ -0,0 +1,22 @@
+#include "pitches.h"
+
+const int buzzerPin = 8;
+const int inPin = 9;  
+int val;
+int notes[] = {
+  NOTE_A4, NOTE_B4, NOTE_C3,NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, NOTE_B3, NOTE_C4
+};
+
+void setup() {
+  pinMode(inPin, INPUT);
+}
+
+void loop() {
+   val = digitalRead(inPin);
+   if(val>0){ 
+    for (int i = 0; i < 10; i++) {
+      tone(buzzerPin, notes[i], 500);
+      delay(500);
+    }
+   }
+}
